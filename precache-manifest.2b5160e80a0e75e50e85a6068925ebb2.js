f8da5031a82887b3909a9f6a2ca78fa6ebc04e5f
Bonny <mntdragon96@gmail.com>
2019-06-24 17:46:36 +0300

    Updates

---
+++ precache-manifest.2b5160e80a0e75e50e85a6068925ebb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea9c9932da953ef972ed3c49056b470",
+    "url": "/React-portfolio-basic/index.html"
+  },
+  {
+    "revision": "c1109e76e59d1c0d0317",
+    "url": "/React-portfolio-basic/static/css/main.94ad8322.chunk.css"
+  },
+  {
+    "revision": "6651997f3a20d092ff83",
+    "url": "/React-portfolio-basic/static/js/2.bb0f9e6e.chunk.js"
+  },
+  {
+    "revision": "c1109e76e59d1c0d0317",
+    "url": "/React-portfolio-basic/static/js/main.5b25957c.chunk.js"
+  },
+  {
+    "revision": "901464270d16ace2d3b3",
+    "url": "/React-portfolio-basic/static/js/runtime~main.26bc63a3.js"
+  }
+]);
